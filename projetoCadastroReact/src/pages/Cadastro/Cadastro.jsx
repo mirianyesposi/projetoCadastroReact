09597df4bcf94ce73b8486225400f49ab8474fcf
Miriany Esposi Ferreira Nunes <108499751+mirianyesposi@users.noreply.github.com>
2024-04-12 21:17:30 -0300

    update

--- projetoCadastroReact/src/pages/Cadastro/Cadastro.jsx
+++ projetoCadastroReact/src/pages/Cadastro/Cadastro.jsx
@@ -1,8 +1,15 @@
-
+// eslint-disable-next-line no-unused-vars
+import React from "react"
 
 export default function Cadastro (){
     return(
         <>
+            <section>
+                <h1>Cadastro</h1>
+                <input type="text" placeholder="Nome" />
+                <input type="text" placeholder="Nome" />
+
+            </section>
         </>
     )
 }
